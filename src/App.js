dfa2d7a5609e0f9010dddf3df3b29d7c1c94d22e
jonathanj101 <jonatjimen@gmail.com>
2020-11-22 17:09:03 -0500

    figured a way to GET HTTP request to get each character correspondent homeworld.. now working to get their species type back. so far getting an array instead of the url
    figured a way to GET HTTP request to get each character correspondent homeworld.. now working to get their species type back. so far getting an array instead of the url

--- src/App.js
+++ src/App.js
@@ -16,17 +16,39 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const peopleData = await axios.get('https://swapi.dev/api/people')
-    const homeWorld = await axios.get('https://swapi.dev/api/planets/')
-    console.log(peopleData.data.results)
-    peopleData.data.results.map(characterData => {
-      const peopleState = this.state.people
-      peopleState.push(characterData)
-      console.log(homeWorld)
-      this.setState({
-        people: peopleState
+    // const peopleData = await axios.get('https://swapi.dev/api/people')
+    // console.log(peopleData.data.results)
+    // peopleData.data.results.map(characterData => {
+    //   // const homeWorld = await axios.get('https://swapi.dev/api/planets/')
+    //   const peopleState = this.state.people
+    //   peopleState.push(characterData)
+    //   // console.log(homeWorld)
+    //   this.setState({
+    //     people: peopleState
+    //   })
+    // })
+    try {
+      const peopleData = await axios.get('https://swapi.dev/api/people')
+      peopleData.data.results.map(async characterData => {
+        // console.log(characterData)
+        // console.log(await characterData.homeworld, characterData)
+        const characterHomeWorld = await axios.get(characterData.homeworld)
+        // console.log(characterHomeWorld.data)
+        const characterSpecies = await axios.get(characterData.species)
+        console.log(characterSpecies)
+        const peopleState = this.state.people
+        // peopleState.push({ characterData, characterHomeWorld, characterSpecies })
+        peopleState.push({ people: characterData, homeworld: characterHomeWorld.data.name, species: characterSpecies.data.results })
+        this.setState({
+          people: peopleState
+        })
       })
-    })
+    }
+
+    catch (err) {
+      console.log(err, 'not successful')
+    }
+
 
     console.log(this.state)
   }
