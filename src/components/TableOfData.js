3a8ffa9b3482682e4cf325e082c5e4be6ad43a89
jonathanj101 <jonatjimen@gmail.com>
2020-11-24 23:12:42 -0500

    pagination deleted, to create  it's own component
    pagination deleted, to create  it's own component

--- src/components/TableOfData.js
+++ src/components/TableOfData.js
@@ -1,12 +1,9 @@
+import React from 'react'
 import { Table } from 'react-bootstrap'
 
 const TableData = (props) => {
-    // console.log(props)
     const peopleState = props.passingData
-    // const speciesState = props.passingData.species
-    // const planetState = props.passingData.planet
     const looping = peopleState.map((characters, num) => {
-        // console.log(characters, num)
         return (
             <tr key={num} >
                 <td>{characters.people.name}</td>
@@ -20,26 +17,28 @@ const TableData = (props) => {
     })
 
     return (
-        <Table
-            className='main-table text-light text-center w-75 mx-auto'
-            striped
-            bordered
-            hover
-            variant="dark"  >
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Birth Date</th>
-                    <th>Height</th>
-                    <th>Mass</th>
-                    <th>Homeworld</th>
-                    <th>Species</th>
-                </tr>
-            </thead>
-            <tbody>
-                {looping}
-            </tbody>
-        </Table>
+        <div>
+            <Table
+                className='main-table text-light text-center w-75 mx-auto'
+                striped
+                bordered
+                hover
+                variant="dark"  >
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Birth Date</th>
+                        <th>Height</th>
+                        <th>Mass</th>
+                        <th>Homeworld</th>
+                        <th>Species</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {looping}
+                </tbody>
+            </Table>
+        </div>
     )
 }
 
