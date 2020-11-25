20a1910bf09f418de969c062cf1c042600f91b96
jonathanj101 <jonatjimen@gmail.com>
2020-11-25 01:12:50 -0500

    code is broken. react keeps fetching same data over and over agauin
    code is broken. react keeps fetching same data over and over agauin

--- src/App.js
+++ src/App.js
@@ -15,41 +15,59 @@ class App extends Component {
       pageNumber: ''
     }
     this.handleChange = this.handleChange.bind(this)
-  }
+    this.handleRequests = this.handleRequests.bind(this)
 
+  }
   async componentDidMount() {
-    try {
-      const peopleData = await axios.get(`https://swapi.dev/api/people/?page=${this.state.pageNumber}`)
-      peopleData.data.results.map(async characterData => {
-        const characterHomeWorld = await axios.get(characterData.homeworld)
-        const characterSpecies = await axios.get(characterData.species)
-        let isHuman = !characterSpecies.data.name ? characterData.species = 'Human' : characterData.species = characterSpecies.data.name
-        const peopleState = this.state.people
-        peopleState.push({
-          people: characterData,
-          homeworld: characterHomeWorld.data.name,
-          species: isHuman,
-        })
-        this.setState({
-          people: peopleState,
-          count: peopleData.data.count
-        })
-      })
-    }
-    catch (err) {
-      console.log(err, 'not successful')
-    }
+    let wait = await this.handleRequests()
   }
 
-  // componentDidUpdate() {
-  //   console.log(this.state.pageNumber)
-  // }
+  async componentDidUpdate() {
+    let wait = await this.handleRequests()
+  }
 
   handleChange = (e) => {
     const { name } = e.target
     this.setState({
       pageNumber: name
     })
+  }
+
+  handleRequests = async () => {
+    let pageNumber = this.state.pageNumber
+    console.log(pageNumber)
+
+    let request = await axios.get(`https://swapi.dev/api/people/?page=${pageNumber}`)
+    // let request = await axios.get(`https://swapi.dev/api/people/`)
+
+    console.log(request)
+    try {
+
+      if (pageNumber) {
+        request.data.results.map(async characterData => {
+          console.log(characterData)
+          const characterHomeWorld = await axios.get(characterData.homeworld)
+          const characterSpecies = await axios.get(characterData.species)
+          let isHuman = !characterSpecies.data.name ? characterData.species = 'Human' : characterData.species = characterSpecies.data.name
+          const peopleState = this.state.people
+          peopleState.push({
+            people: characterData,
+            homeworld: characterHomeWorld.data.name,
+            species: isHuman,
+          })
+          this.setState({
+            people: peopleState,
+            count: request.data.count
+          })
+        })
+
+      } else {
+        console.log('nope')
+      }
+    }
+    catch (error) {
+      console.log(error)
+    }
 
   }
 
