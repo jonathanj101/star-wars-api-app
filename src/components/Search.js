e2cf863696e8172af7f285a50576cc6bcf4c4653
jonathanj101 <jonatjimen@gmail.com>
2020-11-26 23:03:16 -0500

    updating search component, lifting props up
    updating search component, lifting props up

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,28 +1,41 @@
+import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
 import starWarsImg from '../image/star-wars-logo.jpg'
 
-const Search = () => {
-    return (
-        <div>
-            <Card
-                className="w-25 h-25 m-5 mx-auto">
-                <Card.Img
-                    src={starWarsImg} />
-            </Card>
-            <div className="input-group m-5 mx-auto w-50">
-                <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Search" />
-                <div className="input-group-append">
-                    <button
-                        className="btn btn-outline-secondary bg-dark text-light" type="button">
-                        Search
-                </button>
+class Search extends Component {
+    constructor() {
+        super()
+        this.state = {
+            text: ''
+        }
+    }
+
+    onChange = e => {
+        const { value } = e.target
+        this.setState({ text: value })
+        this.props.searchCharacter(this.state.text)
+    }
+    render() {
+        return (
+            <div>
+                <Card
+                    className="w-25 h-25 m-5 mx-auto">
+                    <Card.Img
+                        src={starWarsImg} />
+                </Card>
+                <div className="input-group m-5 mx-auto w-50">
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={this.state.text}
+                        onChange={this.onChange}
+                        placeholder="Search a character by their name" />
+                    <div className="input-group-append">
+                    </div>
                 </div>
-            </div>
-        </div >
-    )
+            </div >
+        )
+    }
 }
 
 export default Search
