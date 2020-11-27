a88928f53bbff78d0ab1f5b03c6eecfe183f0ea1
jonathanj101 <jonatjimen@gmail.com>
2020-11-27 00:47:03 -0500

    passing up state data when button is click
    passing up state data when button is click

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,39 +1,50 @@
+
 import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
 import starWarsImg from '../image/star-wars-logo.jpg'
 
 class Search extends Component {
-    constructor() {
-        super()
-        this.state = {
-            text: ''
-        }
+    state = {
+        text: ''
     }
 
-    onChange = e => {
+    onChangee = e => {
         const { value } = e.target
         this.setState({ text: value })
+    }
+
+    onSubmit = () => {
         this.props.searchCharacter(this.state.text)
+        this.setState({
+            text: ''
+        })
     }
     render() {
         return (
             <div>
-                <Card
-                    className="w-25 h-25 m-5 mx-auto">
-                    <Card.Img
-                        src={starWarsImg} />
-                </Card>
-                <div className="input-group m-5 mx-auto w-50">
-                    <input
-                        className="form-control"
-                        type="text"
-                        value={this.state.text}
-                        onChange={this.onChange}
-                        placeholder="Search a character by their name" />
-                    <div className="input-group-append">
+                <div>
+                    <Card
+                        className="w-25 h-25 m-5 mx-auto">
+                        <Card.Img
+                            src={starWarsImg} />
+                    </Card>
+                    <div className="input-group m-5 mx-auto w-50">
+                        <input
+                            className="form-control"
+                            type="text"
+                            value={this.state.text}
+                            onChange={this.onChangee}
+                            placeholder="Type a character name then press search..." />
+                        <div className="input-group-append">
+                            <button
+                                onClick={this.onSubmit}
+                                className="btn btn-outline-secondary bg-dark text-light" type="button">
+                                Search
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </div >
+                </div >
+            </div>
         )
     }
 }
